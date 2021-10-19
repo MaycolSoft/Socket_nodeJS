3c678d7f150f883d4cef5b752580749acdf13bf7
mbaret <m.baret@anonymoussoft.onmicrosoft.com>
2021-10-19 17:06:50 -0400

    enable socket port + 1

--- index.js
+++ index.js
@@ -2,29 +2,10 @@
  
 const express = require('express')
 const app = express();
-const port = 2001
+const port = 3800
  
 
 
-const io = require('socket.io')(3000);
-
-io.on('connection', socket => {
-  // either with send()
-  socket.send('Hello!');
-
-  // or with emit() and custom event names
-  socket.emit('greetings', 'Hey!', { 'ms': 'jane' }, Buffer.from([4, 3, 3, 1]));
-
-  // handle the event sent with socket.send()
-  socket.on('message', (data) => {
-    console.log(data);
-  });
-
-  // handle the event sent with socket.emit()
-  socket.on('salutations', (elem1, elem2, elem3) => {
-    console.log(elem1, elem2, elem3);
-  });
-});
 
 
 
@@ -36,4 +17,27 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
+
+const io = require('socket.io')(port+1);
+
+io.on('connection', socket => {
+  // either with send()
+  socket.send('Hello!');
+
+  
+
+  // handle the event sent with socket.send()
+  socket.on('say_hello', (data) => {
+    // or with emit() and custom event names
+    socket.emit('greetings', 'Hey!', { 'ms': 'jane' }, Buffer.from([4, 3, 3, 1]));
+  });
+
+  // handle the event sent with socket.emit()
+  socket.on('salutations', (elem1, elem2, elem3) => {
+    console.log(elem1, elem2, elem3);
+  });
+
 });
